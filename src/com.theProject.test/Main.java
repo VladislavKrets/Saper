5feb28c3fb2508198582d3dfdedd02ba499978c7
Vladislav <vlad-lollipop@ro.ru>
2016-08-27 16:08:05 +0300

    main() added

---
+++ src/com.theProject.test/Main.java
@@ -0,0 +1,10 @@
+package com.theProject.test;
+
+/**
+ * Created by lollipop on 27.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
