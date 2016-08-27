72fdd954469c4a33168e8d5ab15c76696e119d4e
Vladislav <vlad-lollipop@ro.ru>
2016-08-27 16:18:09 +0300

    main() added JFrame added

--- src/com.theProject.test/Main.java
+++ src/com.theProject.test/Main.java
@@ -1,10 +1,19 @@
 package com.theProject.test;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by lollipop on 27.08.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        JFrame frame = new JFrame("Form");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+
+        frame.setVisible(true);
+        frame.pack();
     }
 }
